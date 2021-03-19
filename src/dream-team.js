ee8bd84e5b8451eb537734564191c993f6abb34c
spice2445 <79026145+spice2445@users.noreply.github.com>
2021-03-19 17:43:44 +0200

    fix: add UpperCase

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,8 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  if(!members || !members.length) return false;
-  let firstLetters = [];
-  members.map(str => firstLetters.push(str[0]))  
-  return firstLetters.toUpperCase().sort().join("")
+    if(!members || !members.length) return false;
+    let firstLetters = [];
+     members.map(str => firstLetters.push(str[0]).toUpperCase())  
+    return firstLetters.sort().join("");
 };
