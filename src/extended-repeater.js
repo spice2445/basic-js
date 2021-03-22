6082d13a2795f92498122a839ea1bb47e1d4d725
spice2445 <79026145+spice2445@users.noreply.github.com>
2021-03-22 23:06:37 +0200

    feat: finish task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+  let answer = (str === null) ? 'null' : str+'';
+  let addition = (options.addition === undefined) ? '': (options.addition === null) ? 'null' : options.addition+'';
+  let answer_total = answer,
+      addition_total = addition;
+
+  if (options.separator === undefined) {options.separator = '+'}
+  if (options.additionSeparator === undefined) {options.additionSeparator = '|'}
+
+  for (let j = 0; j < options.additionRepeatTimes-1; j++) {
+    addition_total = addition_total + options.additionSeparator.toString() + addition
+  }
+
+  for (let i = 0; i < options.repeatTimes-1; i++) {
+    answer_total = answer_total + addition_total + options.separator.toString()  + answer
+  }
+  return answer_total+addition_total
 };
-  
